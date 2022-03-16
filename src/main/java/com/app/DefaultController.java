b42e23c55f141f43248969afaa392f9756dfaa0f
Omar Hashmi <OmarHashmi@cmail.carleton.ca>
2022-03-16 15:39:35 -0400

    patch
    	Fixed thing

--- src/main/java/com/app/DefaultController.java
+++ src/main/java/com/app/DefaultController.java
@@ -1,23 +1,25 @@
 package com.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class DefaultController {
+    @Autowired
     private final Survey survey;
 
     DefaultController(Survey survey) {
         this.survey = survey;
     }
-
     @RequestMapping("/questions")
     public String greeting(Model model) {
         model.addAttribute("questions", survey.findAll());
         return "questions";
     }
-
     @RequestMapping("/poll")
     public String poll(Model model) {
         // To add variable to thymeleaf edit the command below
@@ -25,12 +27,26 @@ public class DefaultController {
         // model.addAttribute("questions", survey.findAll());
         return "poll";
     }
+    @RequestMapping("/submitAnswers")
+    public RedirectView poll(@RequestParam(value = "answer") String answer,
+                       Model model) {
+        // To add variable to thymeleaf edit the command below
+        // 1st is the variable name, 2nd is the value of the variable
+        Question temp = new Question();
+        temp.setQuestion("What is your name?");
+        temp.setAnswer(answer);
+        survey.save(temp);
+        model.addAttribute("poll", temp);
+
+		return new RedirectView("results");
+    }
 
     @RequestMapping("/results")
     public String results(Model model) {
         // To add variable to thymeleaf edit the command below
         // 1st is the variable name, 2nd is the value of the variable
         // model.addAttribute("questions", survey.findAll());
+        model.addAttribute("results", survey.findAll());
         return "results";
     }
 }
