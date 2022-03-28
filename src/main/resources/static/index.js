1314712043546fa76dea657144f07de375bcfaa5
ttrautrim <trautrim.tbt@outlook.com>
2022-03-28 15:18:50 -0400

    feat: Added SSO updated index to match

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,28 @@
+$.get("/user", function(data) {
+    $("#user").html(data.name);
+    $(".unauthenticated").hide()
+    $(".authenticated").show()
+});
+
+var logout = function() {
+    $.post("/logout", function() {
+        $("#user").html('');
+        $(".unauthenticated").show();
+        $(".authenticated").hide();
+    })
+    return true;
+}
+
+$.ajaxSetup({
+    beforeSend : function(xhr, settings) {
+        if (settings.type == 'POST' || settings.type == 'PUT'
+            || settings.type == 'DELETE') {
+            if (!(/^http:.*/.test(settings.url) || /^https:.*/
+                .test(settings.url))) {
+                // Only send the token to relative URLs i.e. locally.
+                xhr.setRequestHeader("X-XSRF-TOKEN",
+                    Cookies.get('XSRF-TOKEN'));
+            }
+        }
+    }
+});
